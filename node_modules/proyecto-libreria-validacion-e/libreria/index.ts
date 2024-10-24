afbc5a885e066f9f3f92cebb460d2f0eb441ec64
KimberlySalazarB <nohereily.salazar@upch.pe>
2024-10-24 02:42:19 -0500

    actualizacion

---
+++ node_modules/proyecto-libreria-validacion-e/libreria/index.ts
@@ -0,0 +1,9 @@
+export*from './strategy/validacionStrategy';
+export*from './validacion/numberValidacion';
+export*from './validacion/stringValidacion';
+export*from './validacion/dateValidacion';
+export*from './validacion/objectValidacion';
+export*from './strategy/validacionFactory';
+
+
+

3fc4fa1cb37e6b1ce75053c3196985198a5b58ba
KimberlySalazarB <nohereily.salazar@upch.pe>
2024-10-23 23:05:47 -0500

    actualizacion

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ export default{
     preset: 'ts-jest',               // Usa ts-jest para manejar TypeScript
     testEnvironment: 'node',         // Establece el entorno de prueba en Node.js
     testMatch: ['**/*.test.ts'],     // Patrón para buscar archivos de prueba
+    verbose:true,                    // Muestra más detalles durante la ejecución de pruebas
 };
--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
 export default {
-    preset: 'ts-jest',
-    testEnvironment: 'node',  
+    preset: 'ts-jest', //Permite a jest compilar y ejecutar pruebas de TypeScript.
+    testEnvironment: 'node', //entorno de prueba que Jest utilizará. 
     testMatch: ['**/tests/**/*.test.ts'],  // Define dónde buscar archivos de pruebas
     moduleFileExtensions: ['ts', 'js'],  // Extensiones de archivos permitidos
     collectCoverage: true,               // Activar el reporte de cobertura de pruebas
